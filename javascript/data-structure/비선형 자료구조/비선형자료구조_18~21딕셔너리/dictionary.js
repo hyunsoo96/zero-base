281cebeb80fb4b7091c527907ea4f75734f7ad49
Hyunsoo Kim <hyunso_o96@naver.com>
2022-03-11 11:38:57 +0900

    dictionary 구현 2022-03-11

--- javascript/data-structure/비선형 자료구조/비선형자료구조_18~21딕셔너리/dictionary.js
+++ javascript/data-structure/비선형 자료구조/비선형자료구조_18~21딕셔너리/dictionary.js
@@ -24,10 +24,68 @@ Dictionary.prototype.has = function (key) {
   return this.items.hasOwnProperty(key);
 };
 
-let dict = new Dictionary({ age: 19, name: "alice" });
+// set(): 개체(Entity) 추가
+Dictionary.prototype.set = function (key, value) {
+  this.items[key] = value;
+};
+
+// get(): 개체(Entity)의 value 반환
+Dictionary.prototype.get = function (key) {
+  return this.has(key) ? this.items[key] : undefined;
+};
+
+// remove(): 개체(Entity)의 삭제
+Dictionary.prototype.remove = function (key) {
+  if (this.has(key)) {
+    delete this.items[key];
+    return true;
+  }
+  return false;
+};
+
+// keys(): 모든 key 값을 배열 형태로 반환
+Dictionary.prototype.keys = function () {
+  return Object.keys(this.items);
+};
+
+// vlaues(): 모든 value 값을 배열 형태로 반환
+Dictionary.prototype.values = function () {
+  let values = [];
+  for (let k in this.items) {
+    if (this.has(k)) {
+      values.push(this.items[k]);
+    }
+  }
+  return values;
+  // return Object.values(this.items);
+};
+
+Dictionary.prototype.each = function (fn) {
+  for (let k in this.items) {
+    fn(k, this.items[k]);
+  }
+};
+
+function printDictionary(key, value) {
+  console.log(`key: ${key}`);
+  console.log(`value: ${value}`);
+}
+
+let dict = new Dictionary();
+dict.set("age", 19);
+dict.set("name", "alice");
+dict.set("height", 172);
 console.log(dict);
 
-console.log(dict.getBuffer());
-console.log(dict.size());
-dict.clear();
+dict.remove("age");
 console.log(dict);
+
+console.log(dict.has("age"));
+console.log(dict.has("name"));
+console.log(dict.get("age"));
+console.log(dict.get("name"));
+
+console.log(dict.keys());
+console.log(dict.values());
+
+dict.each(printDictionary);
